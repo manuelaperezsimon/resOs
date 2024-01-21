e83445956d32f4c01732613f6f9cc8f71a8b218c
manuelaperezsimon <manuela@swipoo.com>
2024-01-21 17:38:39 +0100

    add extensionsToTreatAsEsm in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from "jest";
 const config: Config = {
   verbose: true,
   testEnvironment: "jsdom",
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
 };
 
 export default config;
