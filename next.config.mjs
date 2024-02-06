e26cc9cf4566f42a265f0ee9d8a9c24d4b00135a
manuelaperezsimon <manuela@swipoo.com>
2024-02-06 16:50:41 +0100

    add dotenv to have .env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
+import { config } from "dotenv";
+
+const { parsed: env } = config();
+
 const nextConfig = {
   swcMinify: true,
   basePath: "/restaurants",
+  env,
 };
 
 export default nextConfig;
